0c786b01e63731c0e11fe03812e3cfeeff8ed117
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-04 10:19:20 +0200

    Add Person Modifier

---
+++ SchoolLibrary/Modifier/PersonModifier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using SchoolLibrary.Costants;
+using SchoolLibrary.Model;
+
+namespace SchoolLibrary.Modifier
+{
+    public class PersonModifier
+    {
+        public bool DeleteById(int IdPerson)
+        {
+            var sql = @"DELETE FROM [dbo].[Person]
+                        WHERE Id=@Id ";
+            using var connection = new SqlConnection(EnvConstants.CONNECTION_STRING);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Id", IdPerson);
+            return command.ExecuteNonQuery() > 0;
+        }
+
+        public bool UpdatePerson(Person person)
+        {
+            var sql = @"UPDATE [dbo].[Person]
+                       SET [Name] = @Name
+                          ,[Surname] = @Surname
+                          ,[BirthDay] = @BirthDay
+                          ,[Gender] = @Gender
+                     WHERE @Id=Id";
+
+            using var connection = new SqlConnection(EnvConstants.CONNECTION_STRING);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Name", person.Name);
+            command.Parameters.AddWithValue("@Surname", person.Surname);
+            command.Parameters.AddWithValue("@BirthDay", person.BirthDay);
+            command.Parameters.AddWithValue("@Gender", person.Gender);
+            command.Parameters.AddWithValue("@Id", person.Id);
+            return command.ExecuteNonQuery() > 0;
+        }
+    }
+}
