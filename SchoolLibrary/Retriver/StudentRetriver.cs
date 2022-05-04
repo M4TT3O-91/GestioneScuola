50d1088272c71eecbf5af622c89547bdcd629e95
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-04 10:14:48 +0200

    Retriver

---
+++ SchoolLibrary/Retriver/StudentRetriver.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using SchoolLibrary.Costants;
+using SchoolLibrary.Model;
+
+namespace SchoolLibrary.Retriver
+{
+    public class StudentRetriver
+    {
+        public IEnumerable<Student> GetStudentrById(int IdStudent)
+        {
+
+            var sql = @"
+                    SELECT [Id]
+                          ,[IdPerson]
+                          ,[Matricola]
+                          ,[DataIscrizione]
+                      FROM [dbo].[Student]
+                        where Id =@IdStudent";
+
+
+            using var connection = new SqlConnection(EnvConstants.CONNECTION_STRING);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@IdStudent", IdStudent);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                yield return new Student
+                {
+                    IdStudent = Convert.ToInt32(reader["Id"]),
+                    Id = Convert.ToInt32(reader["IdPerson"]),
+                    Matricola = reader["Matricola"].ToString(),
+                    DataIscrizione = Convert.ToDateTime(reader["DataIscrizione"]),
+                };
+            }
+        }
+    }
+}
---
+++ SchoolLibrary/Retriver/SubjectRetriver.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using SchoolLibrary.Costants;
+using SchoolLibrary.Model;
+
+namespace SchoolLibrary.Retriver
+{
+    public class SubjectRetriver
+    {
+        public IEnumerable<Subject> GetSubjectById(int IdSubject)
+        {
+
+            var sql = @"
+                    SELECT [Id]
+                          ,[IdName]
+                          ,[Description]
+                          ,[Credits]
+                          ,[Hours]
+                      FROM [dbo].[Subject]
+                        where Id =@IdSubject";
+
+
+            using var connection = new SqlConnection(EnvConstants.CONNECTION_STRING);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@IdSubject", IdSubject);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                yield return new Subject
+                {
+                    IdSubject = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    Description = reader["Description"].ToString(),
+                    Credits = Convert.ToInt32(reader["Credits"]),
+                    Hours = Convert.ToInt32(reader["Hours"]),
+                };
+            }
+        }
+    }
+}
