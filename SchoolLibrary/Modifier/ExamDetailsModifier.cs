40b4ee2e873f72f0f8f7d5acbde85ed2e719c6f5
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-04 10:40:27 +0200

    exam details modifier

---
+++ SchoolLibrary/Modifier/ExamDetailsModifier.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+using SchoolLibrary.Costants;
+using SchoolLibrary.Model;
+
+namespace SchoolLibrary.Modifier
+{
+    public class ExamDetailsModifier
+    {
+        public bool DeleteById(int id)
+        {
+            var sql = @"DELETE FROM [dbo].[ExamDetails]
+                        WHERE Id=@Id ";
+            using var connection = new SqlConnection(EnvConstants.CONNECTION_STRING);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@Id", id);
+            return command.ExecuteNonQuery() > 0;
+        }
+        public bool UpdateExamDetails(ExamDetail examDetails)
+        {
+            var sql = @"UPDATE [dbo].[ExamDetails]
+                       SET [IdExam] = @IdExam
+                          ,[IdStudent] = @IdStudent
+                     WHERE @Id=Id";
+
+            using var connection = new SqlConnection(EnvConstants.CONNECTION_STRING);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@IdExam", examDetails.IdExam);
+            command.Parameters.AddWithValue("@IdStudent", examDetails.IdStudent);
+            command.Parameters.AddWithValue("@Id", examDetails.IdExamDetails);
+            return command.ExecuteNonQuery() > 0;
+        }
+    }
+}
