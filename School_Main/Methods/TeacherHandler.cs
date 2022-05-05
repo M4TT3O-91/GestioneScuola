4b799dacfc4011f305d964969b4be5891c8ec030
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-05 17:21:32 +0200

    refactoring

--- School_Main/Methods/TeacherHandler.cs
+++ School_Main/Methods/TeacherHandler.cs
@@ -2,11 +2,6 @@
 using SchoolLibrary.Modifier;
 using SchoolLibrary.Persister;
 using SchoolLibrary.Retriver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace School_Main.Methods
 {
--- School_Main/Program.cs
+++ School_Main/Program.cs
@@ -4,10 +4,6 @@ using SchoolLibrary.Modifier;
 using SchoolLibrary.Retriver;
 using School_Main.Methods;
 
-var personPersister = new PersonPersister();
-var teacherPersister = new TeacherPersister();
-var studentPersister = new StudentPersister();
-
 var input = "h";
 
 while (input != "exit")
@@ -16,7 +12,7 @@ while (input != "exit")
     {
         case "help":
         case "h":
-            Console.WriteLine("*-------------------------------------------------------------------------------*");
+            Console.WriteLine("*----------------------------- LISTA COMANDI -----------------------------------*");
             Console.WriteLine("|  h, help              -- per vedere i comandi                                 |");
             Console.WriteLine("|  exit                 -- per uscire                                           |");
             Console.WriteLine("|-- Persona --------------------------------------------------------------------|");
@@ -83,127 +79,4 @@ while (input != "exit")
     Console.Write("h per helper, exit per uscire: ");
     input = Console.ReadLine().ToLower();
 
-}
-
-
-
-
-//var person = new Person
-//{
-//    Address = "Largo Colombo",
-//    BirthDay = new DateTime(1975, 6, 30),
-//    Gender = "Male",
-//    Name = "Nicola",
-//    Surname = "Di Bari"
-//};
-
-//var person2 = new Person
-//{
-//    Address = "Vicolo corto",
-//    BirthDay = new DateTime(1990, 4, 30),
-//    Gender = "Female",
-//    Name = "Sara",
-//    Surname = "Rossi"
-//};
-
-
-//person.Id = personPersister.AddPerson(person);
-//person2.Id = personPersister.AddPerson(person2);
-
-//TeacherAdder(teacherPersister, person);
-//StudentAdder(studentPersister, person2);
-
-//printTeacher();
-//printStudent();
-
-
-
-//--------------------------------------------------------------------------
-static void TeacherAdder(TeacherPersister teacherPersister, Person person)
-{
-    var teacher = new Teacher
-    {
-        Address = "Largo Colombo",
-        BirthDay = new DateTime(1975, 6, 30),
-        DataAssunzione = new DateTime(2001, 01, 05),
-        Gender = "Male",
-        Id = person.Id,
-        Name = "Nicola",
-        Surname = "Di Bari",
-        Matricola = "ABDFDSF"
-    };
-
-    var idteacher = teacherPersister.AddTeacher(teacher);
-
-    teacher = new Teacher
-    {
-        Address = "Largo Colombo",
-        BirthDay = new DateTime(1974, 6, 30),
-        DataAssunzione = new DateTime(2001, 01, 05),
-        Gender = "Male",
-        Id = person.Id,
-        Name = "Nicola",
-        Surname = "Bari",
-        Matricola = "ABDFDSF",
-        IdTeacher = idteacher
-    };
-
-    var modifierTeacher = new TeacherModifier();
-    modifierTeacher.UpdateTeacher(teacher);
-}
-
-static void StudentAdder(StudentPersister studentPersister, Person person2)
-{
-    var student = new Student
-    {
-        Address = "Vicolo corto",
-        BirthDay = new DateTime(1990, 4, 30),
-        Gender = "Female",
-        Name = "Sara",
-        Surname = "Rossi",
-        Id = person2.Id,
-        DataIscrizione = new DateTime(2008, 5, 5),
-        Matricola = "ABCDEF",
-    };
-
-    var idStudent = studentPersister.AddStudent(student);
-    student = new Student
-    {
-        Address = "Vicolo corto",
-        BirthDay = new DateTime(1990, 4, 30),
-        Gender = "Female",
-        Name = "Sara",
-        Surname = "Rossi",
-        Id = person2.Id,
-        DataIscrizione = new DateTime(2008, 5, 5),
-        Matricola = "ABCDEF",
-        IdStudent = idStudent
-    };
-
-    var studentModifier = new StudentModifier();
-    studentModifier.UpdateStudent(student);
-}
-
-static void printTeacher()
-{
-    Console.WriteLine("TEACHER:");
-    var teacherRetriver = new TeacherRetriver();
-    var teacherList = teacherRetriver.GetAllTeachers().ToList<Teacher>();
-    foreach (var t in teacherList)
-    {
-        Console.WriteLine(t);
-    }
-    Console.WriteLine();
-}
-
-static void printStudent()
-{
-    Console.WriteLine("STUDENT:");
-    var studentRetriver = new StudentRetriver();
-    var studList = studentRetriver.GetAllStudent().ToList<Student>();
-    foreach (var s in studList)
-    {
-        Console.WriteLine(s);
-    }
-    Console.WriteLine();
 }
